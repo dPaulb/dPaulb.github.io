8cb147ca41bef8ba1f73b1dccdb6b02f3fef8b13
dPaulb <choid1806@naver.com>
2017-08-15 20:29:01 +0900

    init

--- packages.sh
+++ packages.sh
@@ -3,3 +3,4 @@ dpkg-deb -bZgzip projects/icons debs
 dpkg-deb -bZgzip projects/popuponstart debs
 dpkg-deb -bZgzip projects/uponstart debs
 dpkg-deb -bZgzip projects/dPaulb debs
+dpkg-deb -bZgzip projects/BatteryView debs
